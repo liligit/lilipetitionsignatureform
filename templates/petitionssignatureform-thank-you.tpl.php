eeb4a1dc176d63486624ec9bd27f9c9b6d4d2f41
lli <lli@ndi.org>
2017-10-13 12:55:45 -0400

    initial commit

---
+++ templates/petitionssignatureform-thank-you.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation for the signature block thank you content.
+ *
+ * Available variables:
+ * - $title: The title.
+ * - $message: The thank you message.
+ */
+?>
+<div class="thank-you">
+  <h2><?php print $title; ?></h2>
+  <p><?php print $message; ?></p>
+</div>
